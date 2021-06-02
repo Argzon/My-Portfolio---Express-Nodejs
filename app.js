e5fa5e1ec3b3123c9848f19bc95b7b1aeb0fde0a
Argzon <argzon01@gmail.com>
2021-06-02 14:55:05 +0200

    Small Modifications

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 })
 
-// Displays projects depending on project id
+/**
+ * Displays projects depending on project id
+ */
 app.get('/project/:id', (req, res) => {
-    // gets id from url
     const projectId = req.params.id;
     const project = projects.find(({ id }) => id === +projectId);
     if (project) {
@@ -27,5 +28,7 @@ app.get('/project/:id', (req, res) => {
     }
   });
 
-
+/**
+ * When starting localhost it will listen to the port 3000
+ */
 app.listen(3000);
