7349431103fb6db76e4c1c6e2a00a32a071920c6
Argzon <argzon01@gmail.com>
2021-06-02 11:19:06 +0200

    Added projects in homepage

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const data = require('./data.json');
+const {projects} = require('./data.json');
 const app = express();
 
 
@@ -8,12 +8,24 @@ app.use('/static', express.static('public'));
 
 
 app.get('/', (req,res) => {
-    res.render('index', {data}.projects);
+    res.render('index', {projects});
 })
 
 app.get('/about', (req, res) => {
     res.render('about');
 })
+
+// Displays projects depending on project id
+app.get('/project/:id', (req, res) => {
+    // gets id from url
+    const projectId = req.params.id;
+    const project = projects.find(({ id }) => id === +projectId);
+    if (project) {
+      res.render('project', { project });
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
 
 app.listen(3000);
