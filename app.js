1bbc7ef37604c9aac6a1713d194e736fdaeb863c
Argzon <argzon01@gmail.com>
2021-06-03 07:45:56 +0200

    Handled errors and added description on first project

--- app.js
+++ app.js
@@ -19,31 +19,41 @@ app.get('/about', (req, res) => {
  * Displays projects depending on project id
  */
 app.get('/project/:id', (req, res, next) => {
-    const projectId = req.params.id;
-    const project = projects.find(({ id }) => id === +projectId);
-    if (project) {
-      res.render('project', { project });
-    } else {
-      const err = new Error();
-      res.status = 404;
-      res.render('page-not-found');
-      err.message = 'Project not found';
-      next(err);
-    }
-  });
-
-
-  app.get('/', (req, res, next) => {
-
-    // Log out custom error handler indication
-    console.log('Custom error route called');
-  
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
+  if (project) {
+    res.render('project', { project });
+  } else {
     const err = new Error();
-    err.message = `Custom 500 error thrown`
-    res.render('error');
-    err.status = 500;
-    throw err;
-  });
+    err.status = 404;
+    res.render('page-not-found');
+    err.message = 'Project not found';
+    next(err);
+  }
+});
+
+//404 error handler
+app.use((req, res, next) => {
+  const err = new Error('Page not found');
+  err.status = 404;
+  console.log(`404: The page you're looking for doesn't exist.`)
+  next(err);
+});
+
+//Global error handler
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  if (err.status === 404) {
+    res.render('page-not-found', { err });
+  } else {
+    res.status(err.status || 500);
+    err.message = err.message || `Something went wrong on the server.`;
+    console.log(`${err.status}: Something went wrong on the server.`)
+    res.render('error', { err });
+  }
+});
+
 /**
  * When starting localhost it will listen to the port 3000
  */
